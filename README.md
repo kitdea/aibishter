419dac8cc1a1892b0ab568552a91f3fab58753d0
kitdea <francismarrosales@gmail.com>
2021-01-15 10:30:45 +0800

    improved many functionalities

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# aibishter
+# aibishterengineeringservices
+# This is a Company owned by my friends.
+# Most of the images and content owned by the company. 
+# Some images belongs to www.unsplash.com CTTO
+# This is not yet finished i have a lot of work to do. I will try to commit everyday to improved and to fix functionalities.
+# This is my personal project and I have the 
--- saas/js/script.js
+++ saas/js/script.js
@@ -1,22 +1,12 @@
 
-const hamburger = document.getElementsByClassName('toggle')[0];
-const dadNavbar = document.getElementsByClassName('nav-menu')[0];
+// const hamburger = document.getElementsByClassName('toggle')[0];
+// const dadNavbar = document.getElementsByClassName('nav-menu')[0];
 
-hamburger.addEventListener('click', () => {
-    dadNavbar.classList.toggle('show');
+// hamburger.addEventListener('click', () => {
+//     dadNavbar.classList.toggle('show');
 
-});
+// });
 
-
-// let mainNavLinks = document.querySelectorAll("nav ul li a");
-
-// mainNavLinks.forEach(link => {
-//     link.addEventListener("click", event => {
-//       event.preventDefault();
-//       let target = document.querySelector(event.target.hash);
-//       target.scrollIntoView({
-//         behavior: "smooth",
-//         block: "start"
-//       });
-//     });
-//   });
+$('.toggle').click(function(){
+    $('.nav-menu').slideToggle(180);
+  });
