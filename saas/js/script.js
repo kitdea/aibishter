2ac9255a79875a7cb0df47c463dd33263cee9e62
kitdea <francismarrosales@gmail.com>
2021-03-14 08:46:03 +0800

    menu not yet fixed

--- saas/js/script.js
+++ saas/js/script.js
@@ -1,12 +1,3 @@
-
-// const hamburger = document.getElementsByClassName('toggle')[0];
-// const dadNavbar = document.getElementsByClassName('nav-menu')[0];
-
-// hamburger.addEventListener('click', () => {
-//     dadNavbar.classList.toggle('show');
-
-// });
-
 $('.toggle').click(function(){
     $('.nav-menu').slideToggle(180);
   });
